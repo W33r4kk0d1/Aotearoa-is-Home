f41ad83ae838c9cf69d6e3cc86ec05ef28209e0c
Weerakkodi Ranasinghe <weerakkodi.ranasinghe01@whitireianz.ac.nz>
2026-08-19 06:21:07 +1200

    Add project files.

---
+++ Aotearoa is Home/Models/SettlementInformation.cs
@@ -0,0 +1,13 @@
+﻿namespace Aotearoa_is_Home.Models
+{
+    public class SettlementInformation
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Topic { get; set; }
+    }
+}
